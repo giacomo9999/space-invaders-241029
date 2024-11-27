1d66f6e976b87cef1ea5e407937d63661083acd3
James Gary <gmarchesi888@gmail.com>
2024-11-27 12:02:40 -0500

    Rebuilt bullet code from notes.

--- script.js
+++ script.js
@@ -27,6 +27,45 @@ class Player {
         }
         this.x += this.speed
     }
+    shoot() {
+        console.log('Shooting!')
+        const bullet = this.game.getBullet()
+        if (bullet) {
+            bullet.activate(this.x, this.y)
+        }
+    }
+}
+
+class Bullet {
+    constructor() {
+        this.width = 5
+        this.height = 20
+        this.x = 0
+        this.y = 0
+        this.inUse = false
+        this.speed = 20
+    }
+    draw(context) {
+        if (this.inUse) {
+            context.fillRect(this.x, this.y, this.width, this.height)
+        }
+    }
+    update() {
+        if (this.inUse) {
+            this.y -= this.speed
+            if (this.y < -10) {
+                this.inUse = false
+            }
+        }
+    }
+    activate(x, y) {
+        this.x = x
+        this.y = y
+        this.inUse = true
+    }
+    deactivate() {
+        this.inUse = false
+    }
 }
 
 class Game {
@@ -34,22 +73,46 @@ class Game {
         this.canvas = canvas
         this.player = new Player(this)
         this.keyInputs = []
+        this.bulletsPool = []
+        this.numberOfBullets = 10
+        this.fillBulletsPool()
+
         document.addEventListener('keydown', (e) => {
             if (this.keyInputs.indexOf(e.key) === -1) {
                 this.keyInputs.push(e.key)
+            }
+            if (e.key === '1') {
+                this.player.shoot()
             }
             console.log(this.keyInputs)
         })
         document.addEventListener('keyup', (e) => {
             if (this.keyInputs.indexOf(e.key) !== -1) {
-                this.keyInputs.splice(this.keyInputs.indexOf(e.key), 1)
+                // this.keyInputs.splice(this.keyInputs.indexOf(e.key), 1)
+                this.keyInputs.length = 0
             }
             console.log(this.keyInputs)
         })
     }
+    fillBulletsPool() {
+        for (let i = 0; i < this.numberOfBullets; i++) {
+            this.bulletsPool.push(new Bullet(this))
+        }
+    }
+    getBullet() {
+        for (let i = 0; i < this.bulletsPool.length; i++) {
+            if (!this.bulletsPool[i].inUse) {
+                return this.bulletsPool[i]
+            }
+        }
+    }
     render(context) {
         this.player.draw(context)
         this.player.update()
+        this.bulletsPool.forEach((bullet) => {
+            bullet.draw(context)
+            bullet.update()
+        })
     }
 }
 
